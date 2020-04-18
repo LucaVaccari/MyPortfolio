34d525fed12f16bb77651d01a822950f783cad7e
Luca Vaccari <lucavaccari03@gmail.com>
2020-04-18 14:52:59 +0200

    Sidebar

--- js/informationParser.js
+++ js/informationParser.js
@@ -1,14 +1,12 @@
-async function openJSON(fileName)
-{
-    const response = await fetch(fileName);
-    const json = await response.json();
-    return json;
+async function openJSON(fileName) {
+	const response = await fetch(fileName);
+	const json = await response.json();
+	return json;
 }
 
-async function load()
-{
-    let info  = await openJSON("personalInfo.json");
-    console.log(info);
+async function load() {
+	let info = await openJSON("personalInfo.json");
+	console.log(info);
 }
 
 load();
