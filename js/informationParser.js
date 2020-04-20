d141220b8f8b2d44b42a17278cbb881c233f55e8
Luca Vaccari <lucavaccari03@gmail.com>
2020-04-20 08:51:15 +0200

    Information loading

--- js/informationParser.js
+++ js/informationParser.js
@@ -4,9 +4,23 @@ async function openJSON(fileName) {
 	return json;
 }
 
+function setHTML(id, text) {
+	document.getElementById(id).innerHTML = text;
+}
+function getAge(birthDate) {
+	let date = new Date(birthDate.year, birthDate.month, birthDate.day);
+	//the division convert from milliseconds to years
+	return parseInt((Date.now() - date) / 31536000000);
+}
+
 async function load() {
 	let info = await openJSON("personalInfo.json");
 	console.log(info);
+	setHTML("title", info.firstName + " " + info.lastName);
+	setHTML("description", info.description);
+	setHTML("first-name", info.firstName);
+	setHTML("last-name", info.lastName);
+	setHTML("age", getAge(info.birthDate));
 }
 
 load();
